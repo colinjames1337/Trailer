eae64b357407e6e158d99daae8fb12b690f1c720
Colin Malarchuk <colinm1337@Colins-MBP.fios-router.home>
2020-08-11 14:42:13 -0400

    initial xcode project creation

---
+++ Trailer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trailer
+//
+//  Created by Colin Malarchuk on 8/11/20.
+//  Copyright © 2020 Colin Malarchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
